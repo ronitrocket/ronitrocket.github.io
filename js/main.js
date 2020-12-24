eeb82e3564f2c7f1bca255e11aea2c7d24424b64
Ronitrocket <ronitrocket98@gmail.com>
2020-12-23 19:57:39 -0500

    asdas

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+
 const text = [
-  '\n\n\nPlay my games!',
-  '\n\n\nCheck out the downloads!',
-  '\n\n\n..Find any secrets :)',
-  '\n\n\nCheck out my YouTube and Github!',
+    '\n\n\nPlay my games!',
+    '\n\n\nCheck out the downloads!',
+    '\n\n\n..Find any secrets :)',
+    '\n\n\nCheck out my YouTube and Github!',
 ];
 
 let currentText = -1;
 
+for (;;) {
+    for (i = 0; i < text.length; i++) {
+        const string = "";
+        for (x = 0; x < text[i].length; x++) {
+            string = string + charAt(text[i].charAt(x));
+            document.getElementById('textCarousel').innerHTML = string;
+            sleep(100);
+        }
+        sleep(4000);
+        for (y = 0;  < text[i].length; y++) {
+            string = string.substring(0, string.length - 1);
+            document.getElementById('textCarousel').innerHTML = string;
+            sleep(100);
+        }
+    }
+}
 
-const textInterval = setInterval(() => {
-  document.getElementById('textCarousel').innerHTML = text[(++currentText) % text.length];
-}, 5000);
+
